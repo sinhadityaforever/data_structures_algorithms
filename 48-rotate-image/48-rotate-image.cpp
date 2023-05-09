9a3f6127be9691ce80cd541d47c77d79738ffbd2
Aditya Sinha <sinhadityaforever@gmail.com>
2023-05-09 06:58:04 +0530

    Time: 0 ms (100.00%), Space: 7 MB (73.74%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -6,7 +6,7 @@ public:
         //transpose
         int count=0;
         for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
+            for(int j=i+1; j<n; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
             count--;
