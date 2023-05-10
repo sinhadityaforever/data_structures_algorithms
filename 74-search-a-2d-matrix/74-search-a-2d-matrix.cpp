a2a8bf322e38f009bcfcfc1c77a18fc630271c3b
Aditya Sinha <sinhadityaforever@gmail.com>
2023-05-10 06:06:36 +0530

    Time: 5 ms (53.41%), Space: 9.4 MB (77.68%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,37 +3,23 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        
-        vector<int> v;
-        // for(auto i: matrix){
-        //     for(auto j: i){
-        //         v.push_back(j);
-        //     }
-        // }
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                v.push_back(matrix[i][j]);
-            }
-        }
-        
         int s =0;
-        int e = m*n-1;
+        int e = (m*n-1);
         int mid = s+(e-s)/2;
+        
         while(s<=e){
-            if(target==v[mid]){
+            if(matrix[mid/n][mid%n]==target){
                 return true;
             }
-            else if(target<v[mid]){
+            else if(matrix[mid/n][mid%n]>target){
                 e=mid-1;
             }
             else{
                 s=mid+1;
             }
-            mid=s+(e-s)/2;
+            mid= s+(e-s)/2;
         }
         return false;
         
-         
     }
 };
