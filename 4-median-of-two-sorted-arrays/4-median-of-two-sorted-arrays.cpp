e8213789457fd7541215dd542731d0294d06b46f
Aditya Sinha <sinhadityaforever@gmail.com>
2023-05-19 22:32:24 +0530

    Time: 35 ms (86.31%), Space: 89.4 MB (69.34%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
-   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    if (nums1.size() > nums2.size()) {
-        return findMedianSortedArrays(nums2, nums1);
-    }
-    int m = nums1.size();
-    int n = nums2.size();
-    int low = 0;
-    int high = m;
-    while (low <= high) {
-        int partitionNums1 = (low + high) / 2;
-        int partitionNums2 = (m + n + 1) / 2 - partitionNums1;
-        int maxLeftNums1 = (partitionNums1 == 0) ? INT_MIN : nums1[partitionNums1 - 1];
-        int minRightNums1 = (partitionNums1 == m) ? INT_MAX : nums1[partitionNums1];
-        int maxLeftNums2 = (partitionNums2 == 0) ? INT_MIN : nums2[partitionNums2 - 1];
-        int minRightNums2 = (partitionNums2 == n) ? INT_MAX : nums2[partitionNums2];
-        if (maxLeftNums1 <= minRightNums2 && maxLeftNums2 <= minRightNums1) {
-            if ((m + n) % 2 == 0) {
-                return (double)(max(maxLeftNums1, maxLeftNums2) + min(minRightNums1, minRightNums2)) / 2;
-            } else {
-                return (double)max(maxLeftNums1, maxLeftNums2);
-            }
-        } else if (maxLeftNums1 > minRightNums2) {
-            high = partitionNums1 - 1;
-        } else {
-            low = partitionNums1 + 1;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums2.size()<nums1.size()){
+            return findMedianSortedArrays(nums2, nums1);
         }
+        int n1= nums1.size();
+        int n2 = nums2.size();
+        int s = 0;
+        int e = n1;
+        int mid = (s+e)/2;
+        
+        while(s<=e){
+            int cut1 = mid;
+            int cut2 = (n1+n2+1)/2-cut1;
+            
+            int left1 = cut1 ==0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+            
+            if(left1<=right2 && left2 <=right1){
+                if((n1+n2)%2==0){
+                    return (max(left1, left2) + min(right1, right2))/2.0;
+                }
+                else{
+                    return max(left1, left2);
+                }
+            }
+            else if(left1>right2){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            
+            mid=(s+e)/2;
+        }
+        
+        return 0.0;
     }
-    return -1.0;
-}
-
-
 };
