413b8d6d530977605dd4f4a4c5dd72878e011d21
Aditya Sinha <sinhadityaforever@gmail.com>
2023-06-09 19:57:37 +0530

    Time: 71 ms (5.01%), Space: 286.1 MB (5.19%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     
-    int solve(int i, int j, string s, string t, vector<vector<int>> &dp){
-       if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+    bool solve(string s, string t, int i, int j){
+        if(j<0 && i>=0) return 0;
+        else if(i<0) return 1;
         
-        if(s[i]==t[j]) return dp[i][j]= 1+solve(i-1, j-1, s, t, dp);
-        
-        return dp[i][j]=max(solve(i-1, j, s,t,dp), solve(i, j-1, s,t, dp));
+        if(s[i]==t[j]){
+            return solve(s, t, i-1, j-1);
+        }
+       
+            return solve(s, t, i, j-1);
         
     }
     
     bool isSubsequence(string s, string t) {
-        int m = s.length();
-        int n = t.length();
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        int ans = solve(m-1, n-1, s,t,dp);
-        if(ans==s.length()){
-            return true;
-            
-        }
-        return false;
+        return solve(s,t,s.length()-1, t.length()-1);
     }
 };
