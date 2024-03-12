d7eba153de311fc739c323331c1bf883de657048
Aditya Sinha <sinhadityaforever@gmail.com>
2024-03-12 17:39:08 +0530

    Time: 4 ms (56.77%), Space: 14.7 MB (17.14%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(nums.size()<=1) return;
+        int n = nums.size();
+        int curr = n-1;
+        int bp = 1e9;
+        while(curr!=0){
+            int prev = curr-1;
+            if(nums[prev]<nums[curr]){
+                bp = prev;
+                break;
+            }
+            curr--;
+        }
+        if(bp==1e9){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        for(int i=n-1; i>=bp+1; i--){
+            if(nums[i]>nums[bp]){ swap(nums[i], nums[bp]);
+            break;}
+        }
+        
+        sort(nums.begin()+bp+1, nums.end());
+        return;
+        
+    }
+};
